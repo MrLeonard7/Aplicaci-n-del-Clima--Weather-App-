574e21f14dd015667369e9b1bf301a9688a736e0
David B. <89559408+MrLeonard7@users.noreply.github.com>
2025-04-17 17:04:17 -0500

    Estructura de Componentes

---
+++ weather-app/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SearchBar() {
+return (
+    <p>SearchBar Placeholder</p>
+)
+} 
+
+export default SearchBar
---
+++ weather-app/src/components/WeatherDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function WeatherDisplay() {
+return (
+    <p>Weather Display Placeholder</p>
+)
+} 
+
+export default WeatherDisplay
